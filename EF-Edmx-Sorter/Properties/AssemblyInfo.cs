c87e55f8498257bc7374f5bc6fc7882913af1bf9
Tim Abell <tim@timwise.co.uk>
2015-01-23 10:59:09 +0000

    Reset assembly / project info - start at v0.1.0

--- EF-Edmx-Sorter/Properties/AssemblyInfo.cs
+++ EF-Edmx-Sorter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EFTSQLDocumentation.Generator, see See https://github.com/timabell/ef-document-generator")]
+[assembly: AssemblyTitle("EFTSQLDocumentation.Generator, see See https://github.com/timabell/ef-edmx-sorter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EFTSQLDocumentation.Generator")]
+[assembly: AssemblyProduct("EF-Edmx-Sorter")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
